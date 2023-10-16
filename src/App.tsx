808c0806a88f7e27b98bec4563424b8fcc8e3038
Agostin <agocnv93@gmail.com>
2023-10-16 12:27:51 +0200

    chore: improve small UI and UX stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { ImageDetailModal } from './components/ImageDetailModal'
-import { PiDogFill } from 'react-icons/pi'
 
 import './App.css'
 import { breedKeyToString, parseBreeds } from './utils/functions'
@@ -13,11 +12,11 @@ const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
   
-  const API_BASE_PATH = 'https://dog.ceo/api';
+  const DOG_CEO_API_ENDPOINT = 'https://dog.ceo/api';
 
   const fetchDogsBreeds = async () => {
     try {
-      const response = await fetch(`${API_BASE_PATH}/breeds/list/all`)
+      const response = await fetch(`${DOG_CEO_API_ENDPOINT}/breeds/list/all`)
       const data = await response.json()
 
       if (response.status === 200 && data.status === 'success') {
@@ -30,7 +29,7 @@ const App = () => {
         setFilteredBreeds([])
       }
     } catch (error) {
-      setError('Oops! Something went wrong while loading breeds')
+      setError('Oops! Something went wrong while loading dogs breeds')
       setBreeds([])
       setFilteredBreeds([])
     }
@@ -40,7 +39,7 @@ const App = () => {
     setIsLoading(true)
 
     try {
-      const response = await fetch(`${API_BASE_PATH}/breed/${breed}/images/random`)
+      const response = await fetch(`${DOG_CEO_API_ENDPOINT}/breed/${breed}/images/random`)
       const data = await response.json()
       
       if (response.status === 200 && data.status === 'success') {
@@ -74,27 +73,26 @@ const App = () => {
   }, [])
   
   return (
-    <div className='flex items-center justify-center m-auto h-full bg-gray-200'>
+    <div className='flex items-center justify-center m-auto h-full min-h-screen bg-gray-200'>
       <div className='container m-auto px-4 py-10 lg:max-w-5xl'>
         <div className='flex flex-col items-center justify-between sm:flex-row'>
-          <h1 className='flex items-center text-3xl w-full'>
-            <PiDogFill className="mr-2" size={48} /> Dogs List</h1>
+          <h1 className='flex items-center text-3xl w-full'>🐶 Dogs List 🐾</h1>
           <input className='rounded-md shadow-sm border border-gray-300 outline-none px-3 py-1 w-full h-10 mt-2 mb-4 sm:max-w-xs sm:m-0' type="search" placeholder='Search for a dog breed...' onChange={filterResults} />
         </div>
         
-        {error && <p data-testid="error-wrapper">{error}</p>}
-        
-        {filteredBreeds.length && 
-          <ul className='mt-6 shadow-sm border-gray-300 bg-white py-4 px-8 rounded-xl'>
-            {filteredBreeds?.map(breed =>
-              <li
-                data-testid="dogs-breed-wrapper"
-                key={breed} onClick={()=> showRandomBreedImage(breed)}
-                className='py-3 border-b border-b-gray-300 text-xl cursor-pointer transition-colors duration-300 hover:text-cyan-500 last:border-b-0'>
-                {breedKeyToString(breed)}
-              </li>
-            )}
-          </ul>
+        {
+          error ? <p data-testid="error-wrapper">{error}</p> :
+          filteredBreeds?.length && 
+            <ul className='mt-6 shadow-sm border-gray-300 bg-white py-4 px-8 rounded-xl'>
+              {filteredBreeds?.map(breed =>
+                <li
+                  data-testid="dogs-breed-wrapper"
+                  key={breed} onClick={()=> showRandomBreedImage(breed)}
+                  className='py-3 border-b border-b-gray-300 text-xl cursor-pointer transition-colors duration-300 hover:text-cyan-500 last:border-b-0'>
+                  {breedKeyToString(breed)}
+                </li>
+              )}
+            </ul>
         }
         
         {<ImageDetailModal imageUrl={selectedBreedImg} imageAlt={`A picture of a ${selectedBreed || ''} dog`} isLoading={isLoading} />}
